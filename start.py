79c403b2cd353f65fe198963f99cb0c76156c07a
prxpostern <44132439+prxpostern@users.noreply.github.com>
2021-10-23 14:09:33 +0330

    Add files via upload

--- start.py
+++ start.py
@@ -3,23 +3,26 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyromod import listen
 from urllib.parse import quote_plus, unquote
 import math
-from download_from_url import download_file, get_size
-from file_handler import send_to_transfersh_async, progress
+from helpers.download_from_url import download_file, get_size
+from helpers.file_handler import send_to_transfersh_async, progress
 from hachoir.parser import createParser
 from hachoir.metadata import extractMetadata
-from display_progress import progress_for_pyrogram, humanbytes
-from url_uploader import leecher2
-from video_renamer import video_renamer2
-from vconverter import to_video2
+from helpers.display_progress import progress_for_pyrogram, humanbytes
 import os
 import time
 import datetime
 import aiohttp
 import asyncio
 import mimetypes
-from tools import execute
-from ffprobe import stream_creator
-from thumbnail_video import thumb_creator
+from helpers.tools import execute
+from helpers.ffprobe import stream_creator
+from helpers.thumbnail_video import thumb_creator
+from helpers.url_uploader import leecher2
+from helpers.video_renamer import rnv2
+from helpers.audio_renamer import rna2
+from helpers.file_renamer import rnf2
+from helpers.vconverter import to_video2
+from helpers.media_info import cinfo2
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 API_ID = os.environ.get("API_ID")
@@ -38,25 +41,33 @@ if not os.path.isdir(download_path):
     os.mkdir(download_path)
 
 HELP_TXT = """
-    Please Send Your link like bellow:
-  **URL** | **Custom_File_Name.Extension**
+A Simple Telegram Bot to Upload Files From Direct Links, Convert Document media to Video, and Rename Audio/Video/Document Files.
+
+/upload : reply to your url to start uploading from direct link.
     
-    Example:
-   `http://aaa.bbb.ccc/ddd.eee` | **fff.ggg**
+    `http://aaa.bbb.ccc/ddd.eee` | **fff.ggg**
     or
     `http://aaa.bbb.ccc/ddd.eee`
+
+/c2v : reply to your document To Convert It Into Streamable video File.
     
-    /upload : reply to your url to start uploading from direct link.
+/rnv : reply to your video. Example:
     
-    /c2v : reply to your document To Convert It Into Streamable video File.
+    `/rnv | videoname`
     
-    /rnv : reply to your video `/rename_video | newname`
+/rna : reply to your audio. Example:
+
+    `/rna | audioname | title(optional) | artists(optional)`
+    
+/rnf : reply to your document. Example:
+
+    `/rnv | filename.ext`
 """
 
 @bot.on_message(filters.command(["start"]))
 async def start(bot , m):
     """Send a message when the command /start is issued."""
-    await m.reply_text(text=f"Hi . I Can Upload Your Direct Link to Telegram.\nI Can Convert Document Media to Video.\nI Can Reanme Video Files.\nSee /help for more info!")
+    await m.reply_text(text=f"Hi\n\nSee /help for More Info!")
 
     
 @bot.on_message(filters.command(["help"]))
@@ -64,14 +75,26 @@ async def help(bot , m):
     """Send a message when the command /help is issued."""
     await m.reply_text(text=f"{HELP_TXT}")   
 
-@bot.on_message(filters.command(["rnv"]))
-async def video_renamer1(bot , u):
-    await video_renamer2(bot,u)
+@bot.on_message(filters.private & filters.command(["rnv"]))
+async def rnv1(bot , u):
+    await rnv2(bot,u)
     
-@bot.on_message(filters.command(["c2v"]))
+@bot.on_message(filters.private & filters.command(["rna"]))
+async def rna1(bot , u):
+    await rna2(bot,u)
+    
+@bot.on_message(filters.private & filters.command(["rnf"]))
+async def rnf1(bot , u):
+    await rnf2(bot,u)    
+    
+@bot.on_message(filters.private & filters.command(["c2v"]))
 async def to_video1(bot , u):
     await to_video2(bot , u)
     
+@bot.on_message(filters.private & (filters.audio | filters.document | filters.video))
+async def cinfo1(bot , m):
+    await cinfo2(bot , m)
+
 @bot.on_message(filters.private & filters.command(["upload"]))
 async def leecher1(bot , u):
     await leecher2(bot,u)
