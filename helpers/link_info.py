172b3cf73824ce4c058afcb949dc0f8f1e96d73b
prxpostern <44132439+prxpostern@users.noreply.github.com>
2021-12-05 14:11:40 +0330

    Update link_info.py

--- helpers/link_info.py
+++ helpers/link_info.py
@@ -14,50 +14,25 @@ async def linfo2(bot , m):
   else:
     url = m.text.strip()
     if os.path.splitext(url)[1]:
-      try:
-        r = requests.get(url, allow_redirects=True, stream=True)
-        #r.encoding = 'utf-8'
-        if "Content-Disposition" in r.headers.keys():
-          cfname = r.headers.get("Content-Disposition")
-          cfname = cfname.split("filename=")[1]
-          if '\"' in cfname:
-            cfname = cfname.split("\"")[1]
-        else:
-          cfname = unquote(os.path.basename(url))
-      except RequestException as e:
-        await m.reply_text(text=f"Error:\n\n{e}", quote=True)
-      
-      #fname = re.findall("filename=(.+)", r.headers["Content-Disposition"])[0]
+      cfname = unquote(os.path.basename(url))
       mt = mimetypes.guess_type(str(url))[0]
     else:
       try:
         r = requests.get(url, allow_redirects=True, stream=True)
-        #js = json.loads(r)
-        #r.encoding = 'utf-8'
         if "Content-Disposition" in r.headers.keys():
-          
-          with open('file.json', "w", encoding="utf8") as f:
-            json.dump(dict(r.headers), f, ensure_ascii=False)
-            #f.close()
-          with open('file.json', "r", encoding="utf8") as f:  
-            js = json.load(f)
-          
-
           cfname = r.headers.get("Content-Disposition")
           cfname = cfname.split("filename=")[1]
           if '\"' in cfname:
             cfname = cfname.split("\"")[1]
-          #cfname = unquote(cfname)
-          #mt = mimetypes.guess_type(str(cfname))[0]
         else:
           await m.reply_text(text=f"I Could not Determine The FileType !\nPlease Use Custom Filename With Extension\nSee /help", quote=True)
           return
       except RequestException as e:
         await m.reply_text(text=f"Error:\n\n{e}", quote=True)
+        return
         
   r = requests.get(url, allow_redirects=True, stream=True)
   url_size = int(r.headers.get("content-length", 0))
   url_size = get_size(url_size)
 
-  #await m.reply_text(text=f"📋 Link Info:\n\nFile: `{cfname}`\nMime-Type: `{mt}`\nSize: `{url_size}`\n\nUse /upload as reply to your link, it will upload your link to telegram", quote=True)
-  await m.reply_text(text=f"{js}", quote=True)
+  await m.reply_text(text=f"📋 Link Info:\n\nFile: `{cfname}`\nMime-Type: `{mt}`\nSize: `{url_size}`\n\nUse /upload as reply to your link, it will upload your link to telegram", quote=True)
