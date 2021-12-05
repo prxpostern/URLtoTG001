2720372b76e2fbc0ca559dc2f76545525a5b95bd
prxpostern <44132439+prxpostern@users.noreply.github.com>
2021-12-05 08:35:35 +0330

    Create link_info.py

---
+++ helpers/link_info.py
@@ -0,0 +1,25 @@
+import requests, os, mimetypes
+from helpers.download_from_url import get_size
+
+async def cinfo2(bot , m):
+  
+  if "|" in m.text:
+    url , cfname = m.text.split("|", 1)
+    url = url.strip()
+    cfname = cfname.strip()
+    cfname = cfname.replace('%40','@')
+    mt = mimetypes.guess_type(str(cfname))[0]
+  else:
+    url = m.text.strip()
+    if os.path.splitext(url)[1]:
+      cfname = os.path.basename(url)
+      mt = mimetypes.guess_type(str(url))[0]
+    else:
+      await m.reply_text(text=f"I Could not Determine The FileType !\nPlease Use Custom Filename With Extension\nSee /help", quote=True)
+      return
+  
+  r = requests.get(url, allow_redirects=True, stream=True)
+  url_size = int(r.headers.get("content-length", 0))
+  url_size = get_size(url_size)
+
+  await m.reply_text(text=f"📋 Link Info:\n\nFile: `{cfname}`\nMime-Type: `{mt}`\nSize: `{url_size}`\n\nUse /upload as reply to your link, it will upload your link to telegram", quote=True)
