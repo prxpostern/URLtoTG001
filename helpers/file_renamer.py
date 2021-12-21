f462f22a415d4cc90ddef21921ebd9dd5af1ae19
prxpostern <44132439+prxpostern@users.noreply.github.com>
2021-12-21 09:45:03 +0330

    Update file_renamer.py

--- helpers/file_renamer.py
+++ helpers/file_renamer.py
@@ -1,21 +1,24 @@
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-from urllib.parse import quote_plus, unquote
-import math
-from helpers.download_from_url import download_file, get_size
-from helpers.file_handler import send_to_transfersh_async, progress
-from hachoir.parser import createParser
-from hachoir.metadata import extractMetadata
+#from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+#from urllib.parse import quote_plus, unquote
+#import math
+from helpers.download_from_url import get_size
+#from helpers.file_handler import send_to_transfersh_async, progress
+#from hachoir.parser import createParser
+#from hachoir.metadata import extractMetadata
 from helpers.display_progress import progress_for_pyrogram, humanbytes
 import os, time, datetime, aiohttp, asyncio, mimetypes, logging
-from helpers.tools import execute
-from helpers.ffprobe import stream_creator
-from helpers.thumbnail_video import thumb_creator
+from helpers.tools import execute, clean_up
+#from helpers.ffprobe import stream_creator
+#from helpers.thumbnail_video import thumb_creator
 
 logger = logging.getLogger(__name__)
+status = False
 
 async def rnf2(bot , u):
+  
+  global status
+  file_path = None
   
   if not u.reply_to_message:
     await u.reply_text(text=f"Please Reply To Your Document !\n\nExample:\n**/rnf | filename.ext**\n\nsee /help.", quote=True)
@@ -24,15 +27,13 @@ async def rnf2(bot , u):
   m = u.reply_to_message
   
   if m.video:
-    await m.reply_text(text=f"MimeType: {m.video.mime_type}\n\nUse `/rnv` to rename a video file.\nUse `/c2v` for converting a document to video.\nSee /help", quote=True)
-    return
+    ft = m.video
   elif m.audio:
-    await m.reply_text(text=f"MimeType: {m.audio.mime_type}\n\nUse `/rna` to rename an audio file.\nSee /help", quote=True)
-    return
+    ft = m.audio
   elif m.document:
     ft = m.document
   else:
-    await m.reply_text(text=f"Wrong Filetype !\n\nSee /help", quote=True)
+    await m.reply_text(text=f"Please Reply to (audio-video-document) files !\n\nSee /help", quote=True)
     return
 
   fsize = get_size(ft.file_size)
@@ -49,20 +50,33 @@ async def rnf2(bot , u):
         await m.reply_text(text=f"Type Extension !\n\nExample:\n**/rnf | filename.ext**", quote=True)
         return
       else:
+        tnow = str(datetime.datetime.now())
+        tnow = tnow.replace(' ','_')
+        tnow = tnow.replace('-','_')
+        tnow = tnow.replace(':','_')
+        tnow = tnow.replace('/','_')
+        tnow = tnow.replace('.','_')
+        print("tnow = ", tnow)
+        
         ext = os.path.splitext(newname)[1]
-        tempname = "File_CHATID" + str(m.chat.id) + "_DATE" + str(m.date) + str(ext)
+        oldname = "File_CHATID" + str(m.chat.id) + "_DATE_" + str(tnow) + str(ext)
         if ft.file_name:
             oldname = ft.file_name
             oldname = oldname.replace('%40','@')
             oldname = oldname.replace('%25','_')
             oldname = oldname.replace(' ','_')
-        else:
-            oldname = "File_CHATID" + str(m.chat.id) + "_DATE" + str(m.date) + str(ext)
+
+        print("oldname = ", oldname)
         msg = await m.reply_text(text=f"⬇️ Trying To Download Document", quote=True)
+        
+        #################################################################### Downloading Document
+        status = True
+        logger.info(f"status: {status}")
+        
         c_time = time.time()
         file_path = await bot.download_media(
           m,
-          file_name=tempname,
+          file_name=oldname,
           progress=progress_for_pyrogram,
           progress_args=(
             "⬇️ Downloading Document:",
@@ -71,11 +85,11 @@ async def rnf2(bot , u):
           )
         )
         if not file_path:
+          status = False
+          logger.info(f"status: {status}")
           await msg.edit(f"❌ Downloading Document Failed !")
-          try:
-            os.remove(file_path)
-          except:
-            pass
+          await clean_up(file_path)
+          logger.info(f"Deleted: {file_path}")
           return
         try:
           await msg.edit(f"⬆️ Trying to Upload as Document ...")
@@ -93,17 +107,15 @@ async def rnf2(bot , u):
               c_time
             )
           )
+          status = False
+          logger.info(f"status: {status}")
           await msg.delete()
-          try:
-            os.remove(file_path)
-          except:
-            pass
+          await clean_up(file_path)
+          logger.info(f"Deleted: {file_path}")
         except Exception as e:
           await msg.edit(f"❌ Uploading as Document Failed **Error:**\n\n{e}")
-          try:
-            os.remove(file_path)
-          except:
-            pass
+          await clean_up(file_path)
+          logger.info(f"Deleted: {file_path}")
     else:
-      await m.reply_text(text=f"Try Again !\n\nExample:\n**/rnf | filename.ext**", quote=True)
+      await m.reply_text(text=f"are you kidding me ?\n\nExample:\n`/rnf | filename.ext`\n\nsee /hlep.", quote=True)
       return
