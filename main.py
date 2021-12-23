be08b80280b4e4fe8dc89ca7c3e8144d26b801dc
prxpostern <44132439+prxpostern@users.noreply.github.com>
2021-12-23 18:39:28 +0330

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ class Config:
     DOWNLOAD_DIRECTORY = "Downloads/"
     SP_LIT_ALGO_RITH_M= "hjs"
     MAX_TG_SPLIT_FILE_SIZE = 1.9 * 1024 * 1024 * 1024
-    TG_MAX_FILE_SIZE = 2 * 1024 * 1024 * 1024
+    TG_MAX_FILE_SIZE = 1.9 * 1024 * 1024 * 1024
     
     
 if __name__ == "__main__":
